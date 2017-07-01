9a30753f6d4638d41b51d4a16981090ea3e8ac16
spacejack <spacejack@gmail.com>
2017-06-30 20:28:57 -0400

    Collapsable comments

--- src/ts/freddy/article/comments.ts
+++ src/ts/freddy/article/comments.ts
@@ -12,20 +12,23 @@ function renderCommentTree (comment: Comment, time: number): m.Children {
 	if (!comment || !comment.author) {
 		return null
 	}
-	return m('div.comment',
-		[
-			m('.head',
-				m('span.author', comment.author),
-				m('span.meta',
-					` ${comment.score} points ${age(time - (+comment.created_utc))}`,
-					comment.edited && '*',
-					comment.gilded > 0 && m('span.symbol', m.trust('&#10024;')),
-					comment.controversiality > 0 && m('span.symbol', m.trust('&dagger;'))
-				)
-			),
-			m('.body', comment.body)
-		].concat(
-			comment.replies.map(c => renderCommentTree(c, time) as m.Vnode<any,any>)
-		)
+	return m('.comment',
+		m('.head',
+			{
+				onclick: (e: Event & {redraw?: boolean}) => {
+					(e.currentTarget as HTMLElement).parentElement!.classList.toggle('collapsed')
+					e.redraw = false
+				}
+			},
+			m('span.author', comment.author),
+			m('span.meta',
+				` ${comment.score} points ${age(time - (+comment.created_utc))}`,
+				comment.edited && '*',
+				comment.gilded > 0 && m('span.symbol', m.trust('&#10024;')),
+				comment.controversiality > 0 && m('span.symbol', m.trust('&dagger;'))
+			)
+		),
+		m('.body', comment.body),
+		comment.replies.map(c => renderCommentTree(c, time) as m.Vnode<any,any>)
 	)
 }
